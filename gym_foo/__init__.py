f651f782572230ce83795ce4d67a18834b622a0f
Shreyas Joshi <30567439+ShreyasJoshi7@users.noreply.github.com>
2018-04-16 20:36:47 +0530

    Update __init__.py

--- gym_foo/__init__.py
+++ gym_foo/__init__.py
@@ -1,10 +1,39 @@
-from gym.envs.registration import registry, register, make, spec
+import logging
+from gym.envs.registration import register
+
+logger = logging.getLogger(__name__)
 
 register(
-    id='foo-v0',
+    id='gym_foo-v0',
     entry_point='gym_foo.envs:fooEnv',
+    timestep_limit=1000,
+    reward_threshold=1.0,
+    nondeterministic = True,
 )
+
 register(
-    id='foo-extrahard-v0',
+    id='foo_extrahard_env-v0',
     entry_point='gym_foo.envs:FooExtraHardEnv',
+    timestep_limit=1000,
+    reward_threshold=10.0,
+    nondeterministic = True,
 )
+
+
+
+
+
+
+
+
+
+#from gym.envs.registration import registry, register, make, spec
+
+#register(
+#    id='foo-v0',
+#    entry_point='gym_foo.envs:fooEnv',
+#)
+#register(
+#    id='foo-extrahard-v0',
+#    entry_point='gym_foo.envs:FooExtraHardEnv',
+#)
